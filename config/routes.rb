199304755cbf40219e113acbf3b4551110675007
David <david.njoroge@student.moringaschool.com>
2022-11-24 14:07:37 +0300

    completed create read lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/plants/:id', to: 'plants#show'
+  post '/plants', to: 'plants#create'
+  get '/plants', to: 'plants#index'
 end
