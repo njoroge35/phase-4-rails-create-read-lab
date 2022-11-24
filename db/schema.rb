4511a9cad8267a985872fc78572ece19c68c3324
David <david.njoroge@student.moringaschool.com>
2022-11-24 14:14:21 +0300

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,9 @@ ActiveRecord::Schema.define(version: 2022_11_24_105358) do
   create_table "plants", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "image"
+    t.decimal "price"
   end
 
 end
